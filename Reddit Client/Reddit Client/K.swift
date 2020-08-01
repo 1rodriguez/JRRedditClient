5e9d5c5aea9725d420db69fb938104f65ac15781
Juan Rodriguez <jrodri89@uwo.ca>
2020-08-01 14:56:30 -0400

    Add properties for safe handling of clientID

--- Reddit Client/Reddit Client/K.swift
+++ Reddit Client/Reddit Client/K.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+struct K {
+    static var clientId: String? {
+        let path = Bundle.main.path(forResource: "Secrets", ofType: "plist")
+        
+        if let verifiedPath = path {
+            let url = URL(fileURLWithPath: verifiedPath)
+            
+            do {
+                let data = try Data(contentsOf: url)
+                
+                let plist = try PropertyListSerialization.propertyList(from: data,
+                                                                       options: .mutableContainers,
+                                                                       format: nil)
+                
+                let dictArray = plist as? [[String: String]]
+                
+                if let propertyList = dictArray {
+                    let clientID = propertyList[0]["clientId"]
+                    
+                    return clientID
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+    
+    static var base64Id: String? {
+        if let identification = self.clientId {
+            guard let unicode = identification.data(using: .utf8) else { fatalError() }
+            
+            let encoded = unicode.base64EncodedString()
+            
+            return encoded
+        }
+        return nil
+    }
+}
