29792d51b45c42698ffbdb7c98031f5db325a584
Juan Rodriguez <jrodri89@uwo.ca>
2020-08-01 08:00:17 -0400

    Migrating methods to RequestManager

--- Reddit Client/Reddit Client/Models/RequestManager.swift
+++ Reddit Client/Reddit Client/Models/RequestManager.swift
@@ -10,7 +10,97 @@ import Foundation
 
 struct RequestManager {
     
+    func getIdentity() {
+        let url = URL(string: "https://oauth.reddit.com/api/v1/me")!
+        
+        let clientToken = "586676600006-UEPWWz2BSXvKls31-dyTWMyswxs"
+        let unicode = clientToken.data(using: .utf8)
+        let encoded = unicode!.base64EncodedString()
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("bearer " + clientToken, forHTTPHeaderField: "Authorization")
+        
+        
+        let session = URLSession(configuration: .default)
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if let safeData = data {
+                let json = try? JSONSerialization.jsonObject(with: safeData, options: .allowFragments)
+                
+                if let successfulJSON = json {
+                    print(successfulJSON)
+                }
+            }
+        }
+        dataTask.resume()
+    }
     
+    func refreshToken() {
+        let url = URL(string: "https://www.reddit.com/api/v1/access_token")!
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "refresh_token"),
+            URLQueryItem(name: "refresh_token", value: "586676600006-FCGQNR4pp-3et-5v20YaiIfD05E"),
+        ]
+        
+        let query = components.url!.query
+        
+        let clientId = "jLY0sGcY8DY2iA:"
+        let unicode = clientId.data(using: .utf8)
+        let encoded = unicode!.base64EncodedString()
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("Basic " + encoded, forHTTPHeaderField: "Authorization")
+        request.httpBody = Data(query!.utf8)
+        
+        let session = URLSession(configuration: .default)
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if let safeData = data {
+                let json = try? JSONSerialization.jsonObject(with: safeData, options: .allowFragments)
+                
+                if let successfulJSON = json {
+                    print(successfulJSON)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+    
+    func getToken() {
+        let url = URL(string: "https://www.reddit.com/api/v1/access_token")!
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "authorization_code"),
+            URLQueryItem(name: "code", value: "zoWaXHNVKtxB6uLq_OvcfC3GEYI"),
+            URLQueryItem(name: "redirect_uri", value: "https://juanrodriguez.ca")
+        ]
+        
+        let query = components.url!.query
+        
+        let clientId = "jLY0sGcY8DY2iA:"
+        let unicode = clientId.data(using: .utf8)
+        let encoded = unicode!.base64EncodedString()
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("Basic " + encoded, forHTTPHeaderField: "Authorization")
+        request.httpBody = Data(query!.utf8)
+        
+        let session = URLSession(configuration: .default)
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if let safeData = data {
+                let json = try? JSONSerialization.jsonObject(with: safeData, options: .allowFragments)
+                
+                if let successfulJSON = json {
+                    print(successfulJSON)
+                }
+            }
+        }
+        dataTask.resume()
+    }
     
     
 }
